b24ffce164532798d65e936760f742172370afca
fseoane <fseoane@hotmail.com>
2023-06-21 15:03:54 +0200

    ...

--- install.binaries.sh
+++ install.binaries.sh
@@ -10,13 +10,13 @@ read -r -p "Install default configuration file (pyNotify.conf)? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
         sudo cp pyNotify.conf /opt/pyNotify/
+		echo "ATENTION:"
+		echo "Please configure file /opt/pyNotify/pyNotify.conf with your proper values"
 else
 		echo "Skipped"
 fi
 
 sudo chmod -R 755 /opt/pyNotify
 sudo chown -R root:users /opt/pyNotify
-desktop-file-install --dir=$HOME/.local/share/applications /opt/pyNotify/pyNotify.desktop
+sudo desktop-file-install --dir=$HOME/.local/share/applications /opt/pyNotify/pyNotify.desktop
 
-echo "ATENTION:"
-echo "Please configure /opt/pyNotify/pyNotify.conf with the proper values"
