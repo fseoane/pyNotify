ac04fdada489b623798635d491a8dd58f3225447
fseoane <fseoane@hotmail.com>
2023-06-21 08:08:49 +0200

    v0.3

--- pyNotify.py
+++ pyNotify.py
@@ -4,7 +4,7 @@ import sys
 import configparser
 import asyncio
 import threading
-from gotify import AsyncGotify    # pip install gotify
+from gotify import AsyncGotify  
 import subprocess
 import json
 from playsound import playsound
@@ -13,10 +13,7 @@ import PIL.Image
 import psutil
  
 def checkIfProcessRunning(processName):
-	'''
-	Check if there is any running process that contains the given name processName.
-	'''
-	#Iterate over the all the running process
+	# Check if there is any running process that contains the given name processName.
 	countProcesses = 0
 	for proc in psutil.process_iter():
 		if (proc.name().lower() == processName.lower()):
@@ -28,16 +25,15 @@ def checkIfProcessRunning(processName):
 	return False
 
 
-async def log_push_messages(conf_gotify_url,conf_client_token,conf_notification_sound,conf_notification_icon):
+async def log_push_messages(tray_icon,conf_gotify_url,conf_client_token,conf_notification_sound,conf_notification_icon):
 	global EXIT_REQUESTED
 	async_gotify = AsyncGotify(
 		base_url=conf_gotify_url,
 		client_token=conf_client_token,
 	)
-	print("...listening")
 	async for msg in async_gotify.stream():
 		playsound(conf_notification_sound)
-		subprocess.run(["notify-send", "-u", "normal", "-i", conf_notification_icon, "-t", "3000",msg["title"], msg["message"]],check=True)
+		tray_icon.notify(message=msg["message"],title=msg["title"])
 
 
 def tray_icon_on_clicked(tray_icon, item):
@@ -55,12 +51,22 @@ if __name__ == "__main__":
 	if (SCRIPT_PATH[0]!='/'):
 		PATH_SEPARATOR = '\\'
   
+	pyNotify_version="v0.3"
 	progname = sys.argv[0]
 	processName = progname[progname.rfind(PATH_SEPARATOR)+1:]
-	print ("Checking if {} is already running...".format(processName))
 		
 	if checkIfProcessRunning(processName):
-		print ("{} process already exists. {} seems to be running. Exiting".format(processName,processName))
+		subprocess.run(
+			[
+				"notify-send", 
+				"-u", "normal", 
+				"-i", "error", 
+				"-t", "3000",
+				"pyNotify ERROR", 
+				"{} process already exists. {} seems to be running. Exiting".format(processName,processName)
+			],
+			check=True
+		)
 		sys.exit(1) 
 
 	try:   
@@ -74,22 +80,31 @@ if __name__ == "__main__":
 		conf_notification_sound=SCRIPT_PATH+PATH_SEPARATOR+config['config']['notification_sound']
 		conf_notification_icon=config['config']['notification_icon_name']
 
-		print("Creating tray icon: {}".format(conf_tray_icon))
 		pyNotify_icon=PIL.Image.open(conf_tray_icon)
 
 		tray_icon = pystray.Icon("pyNotify", pyNotify_icon, title="pyNotify", visible=True,
 			menu=pystray.Menu(
+				pystray.MenuItem("About",
+                    pystray.Menu(
+						pystray.MenuItem("     pyNotify {}".format(pyNotify_version),action=None, checked=None, radio=False, default=False, visible=True, enabled=False),
+						pystray.MenuItem('    Fernando Seoane',action=None, checked=None, radio=False, default=False, visible=True, enabled=False),
+						pystray.MenuItem('       Jun 2023',action=None, checked=None, radio=False, default=False, visible=True, enabled=False),
+						pystray.MenuItem("───────────────────────",action=None, checked=None, radio=False, default=False, visible=True, enabled=False),
+						pystray.MenuItem("Conf:  {}".format(SCRIPT_PATH+PATH_SEPARATOR+'pyNotify.conf'),action=None, checked=None, radio=False, default=False, visible=True, enabled=False),
+						pystray.MenuItem("Server:{}".format(conf_gotify_url),action=None, checked=None, radio=False, default=False, visible=True, enabled=False),
+						pystray.MenuItem("Token: {}".format(conf_client_token),action=None, checked=None, radio=False, default=False, visible=True, enabled=False),
+    				)
+                ),
+				pystray.MenuItem("───────────────────────",action=None, checked=None, radio=False, default=False, visible=True, enabled=False),
 				pystray.MenuItem("Quit", tray_icon_on_clicked)
 			)
 		)
 		
-		# Run the icon mainloop in a separate thread
+		# Run the icon mainloop in first thread
 		threading.Thread(target=tray_icon.run).start()
   
-		# Run the gotify listener second thread
-		print ("...connecting to : {} as client: {}".format(conf_gotify_url,conf_client_token))
+		# Run the gotify listener asynchronously in a second thread
 		with asyncio.Runner() as runner:
-			runner.run(log_push_messages(conf_gotify_url,conf_client_token,conf_notification_sound,conf_notification_icon))
-		#asyncio.run(log_push_messages(conf_gotify_url,conf_client_token,conf_notification_sound,conf_notification_icon))
+			runner.run(log_push_messages(tray_icon,conf_gotify_url,conf_client_token,conf_notification_sound,conf_notification_icon))
 	finally:
-		print("Finishing...")
+		sys.exit(0) 
