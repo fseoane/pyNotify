aabbf843eff92f8583ef79897fc7bd1eb1496bb2
fseoane <fseoane@hotmail.com>
2023-06-21 21:21:52 +0200

    ...

--- build.Debian12.sh
+++ build.Debian12.sh
@@ -3,24 +3,24 @@ sudo apt install libcairo2-dev libjpeg-dev libgif-dev
 sudo apt install libgirepository1.0-dev
 
 mkdir -p .env 
-python3 -m venv .env
+python3.11 -m venv .env
 sleep 10
 source .env/bin/activate
 
-python3  -m pip install --upgrade pip setuptools wheel
+python3.11  -m pip install --upgrade pip setuptools wheel
 
-python3 -m pip install gotify
-python3 -m pip install gotify[stream]
-#python3 -m pip install playsound
-python3 -m pip install pyinstaller
-python3 -m pip install pycairo
-python3 -m pip install pygame
-python3 -m pip install pygobject
-python3 -m pip install asyncio
-python3 -m pip install pystray
-python3 -m pip install pillow
-python3 -m pip install websockets
-python3 -m pip install psutil
+python3.11 -m pip install gotify
+python3.11 -m pip install gotify[stream]
+#python3.11 -m pip install playsound
+python3.11 -m pip install pyinstaller
+python3.11 -m pip install pycairo
+python3.11 -m pip install pygame
+python3.11 -m pip install pygobject
+python3.11 -m pip install asyncio
+python3.11 -m pip install pystray
+python3.11 -m pip install pillow
+python3.11 -m pip install websockets
+python3.11 -m pip install psutil
 
 pyinstaller --onefile --windowed pyNotify.py
 
