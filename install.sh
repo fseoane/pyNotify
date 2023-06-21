7861949b431fe140caed6be6663327b7c41fdf28
fseoane <fseoane@hotmail.com>
2023-06-21 14:46:25 +0200

    ...

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ killall pyNotify
 sudo mkdir -p /opt/pyNotify
 sudo cp dist/*  /opt/pyNotify/
 cp /opt/pyNotify/pyNotify.desktop ~/.local/share/applications
+desktop-file-install --dir=$HOME/.local/share/applications /opt/pyNotify/pyNotify.desktop
 sudo chmod -R 755 /opt/pyNotify
 sudo chown -R root:users /opt/pyNotify
 
