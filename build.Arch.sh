f6a8490fd7fc706f9be8b89aeab046cbd9a1de46
fseoane <fseoane@hotmail.com>
2023-06-21 19:47:25 +0200

    ...

--- build.Arch.sh
+++ build.Arch.sh
@@ -4,11 +4,14 @@ mkdir .env
 python -m venv .env
 source .env/bin/activate
 
+python  -m pip install --upgrade pip setuptools wheel
+
 python -m pip install gotify
 python -m pip install gotify[stream]
-python -m pip install playsound
+#python -m pip install playsound
 python -m pip install pyinstaller
 python -m pip install pycairo
+python -m pip install pygame
 python -m pip install pygobject
 python -m pip install asyncio
 python -m pip install pystray
