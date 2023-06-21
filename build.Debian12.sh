22e0141343bbda083a2323e73812533dc3aef644
fseoane <fseoane@hotmail.com>
2023-06-21 21:25:59 +0200

    ...

--- build.Debian12.sh
+++ build.Debian12.sh
@@ -2,9 +2,9 @@ sudo apt install gir1.2-appindicator3-0.1
 sudo apt install libcairo2-dev libjpeg-dev libgif-dev
 sudo apt install libgirepository1.0-dev
 
-mkdir -p .env 
-python3.11 -m venv .env
-source .env/bin/activate
+# mkdir -p .env 
+# python3.11 -m venv .env
+# source .env/bin/activate
 
 python3.11  -m pip install --upgrade pip setuptools wheel
 
@@ -30,5 +30,5 @@ cp pyNotify.desktop dist
 cp install.binaries.sh dist/install.sh
 cp pyNotify.conf dist/pyNotify.conf
 
-deactivate
-rm -rf .env
+# deactivate
+# rm -rf .env
---
+++ build.Devian12.prepare.sh
@@ -0,0 +1,3 @@
+mkdir -p .env 
+python3.11 -m venv .env
+source .env/bin/activate
