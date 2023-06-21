dca99e4c3dd894e35e1c88930dea7af89e51706a
Fernando Seoane <fseoane@hotmail.com>
2023-06-21 12:11:54 +0200

    modifying BUILD  bash files while testing different distros

---
+++ build.Arch.sh
@@ -0,0 +1,27 @@
+sudo pacman - S libappindicator-gtk3
+
+mkdir .env 
+python -m venv .env
+source .env/bin/activate
+
+python -m pip install gotify
+python -m pip install gotify[stream]
+python -m pip install playsound
+python -m pip install pyinstaller
+python -m pip install pycairo
+python -m pip install pygobject
+python -m pip install pystray
+python -m pip install pillow
+python -m pip install websockets
+python -m pip install psutil
+
+pyinstaller --onefile --windowed pyNotify.py
+
+cp notification.ogg dist
+cp notification.svg dist
+cp notification.png dist
+cp pyNotify.desktop dist
+cp pyNotify.conf dist/pyNotify.conf
+
+deactivate
+rm -rf .env
--- build.RHEL.sh
+++ build.RHEL.sh
@@ -1,13 +1,17 @@
+sudo dnf install --assumeyes libappindicator-gtk3
+sudo dnf install --assumeyes cairo-devel pkg-config python3-devel
+sudo dnf install --assumeyes gobject-introspection-devel 
+sudo dnf install --assumeyes cairo-gobject-devel 
 mkdir .env 
 python -m venv .env
 source .env/bin/activate
 
-python -m pip install --upgrade setuptools wheel
-
 python -m pip install gotify
 python -m pip install gotify[stream]
-python -m pip install playsound
+#python -m pip install --use-pep517 playsound
+python -m pip install  playsound
 python -m pip install pyinstaller
+python -m pip install pycairo
 python -m pip install pygobject
 python -m pip install pystray
 python -m pip install pillow
