d983efb85e06e11d87ae9084255c38025cb30d88
fseoane <fseoane@hotmail.com>
2023-06-23 20:01:50 +0200

    v0.5

--- build.4.AUR.sh
+++ build.4.AUR.sh
@@ -4,22 +4,8 @@ mkdir -p .env
 python -m venv .env
 source .env/bin/activate
 
-# python  -m pip install --upgrade pip setuptools wheel
-
-# python -m pip cache purge
-
-# python -m pip install gotify[stream]
-# python -m pip install pyinstaller
-# python -m pip install pygame
-# python -m pip install pygobject
-# python -m pip install asyncio
-# python -m pip install pystray
-# python -m pip install pillow
-# python -m pip install psutil
-
 python -m pip install --requirement requirements.req 
 
 pyinstaller --onefile --windowed pyNotify.py
 
 deactivate
-#rm -rf .env
