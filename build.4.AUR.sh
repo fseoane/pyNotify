3da334cbe3ea3a5b5afb415f289d95217c28a4cb
fseoane <fseoane@hotmail.com>
2023-06-23 19:05:27 +0200

    build script for AUR (to be launched from PKGBUILD)

---
+++ build.4.AUR.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p .env 
+python -m venv .env
+source .env/bin/activate
+
+#python  -m pip install --upgrade pip setuptools wheel
+
+python -m pip cache purge
+
+python -m pip install gotify[stream]
+python -m pip install pyinstaller
+python -m pip install pygame
+python -m pip install pygobject
+python -m pip install asyncio
+python -m pip install pystray
+python -m pip install pillow
+python -m pip install psutil
+
+pyinstaller --onefile --windowed pyNotify.py
+
+deactivate
+rm -rf .env
