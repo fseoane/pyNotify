d0ce83eba4b2a077067bfac6bc468f29ff4d6c24
fseoane <73739110+fseoane@users.noreply.github.com>
2023-06-24 10:20:08 +0200

    v0.5

--- build.Arch.sh
+++ build.Arch.sh
@@ -4,39 +4,14 @@ mkdir .env
 python -m venv .env
 source .env/bin/activate
 
-sudo -S pacman -S libappindicator-gtk3
-
-python  -m pip install --upgrade pip #setuptools wheel
-
-python -m pip uninstall gotify
-python -m pip uninstall gotify[stream]
-python -m pip uninstall pyinstaller
-python -m pip uninstall pycairo
-python -m pip uninstall pygame
-python -m pip uninstall pygobject
-python -m pip uninstall asyncio
-python -m pip uninstall pystray
-python -m pip uninstall pillow
-python -m pip uninstall websockets
-python -m pip uninstall psutil
-
-#python -m pip install gotify
-python -m pip install gotify[stream]
-python -m pip install pyinstaller
-python -m pip install pycairo
-python -m pip install pygame
-python -m pip install pygobject
-python -m pip install asyncio
-python -m pip install pystray
-python -m pip install pillow
-#python -m pip install websockets
-python -m pip install psutil
+python -m pip install -r requirements.req
 
 pyinstaller --onefile --windowed pyNotify.py
 
 cp notification.ogg dist
 cp notification.svg dist
 cp notification.png dist
+cp pyNotify.ver dist
 cp pyNotify.desktop dist
 cp install.binaries.sh dist/install.sh
 cp pyNotify.conf dist/pyNotify.conf
