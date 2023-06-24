5a0b616429b29683fa6f11c05666e569ec6b3c86
fseoane <fseoane@hotmail.com>
2023-06-24 14:09:17 +0200

    ...

---
+++ build.deb.package.sh
@@ -0,0 +1,8 @@
+cp dist/pyNotify DEB.pyNotify.deb/opt/pyNotify/
+cp dist/pyNotify.ver DEB.pyNotify.deb/opt/pyNotify/
+cp dist/pyNotify.desktop DEB.pyNotify.deb/opt/pyNotify/
+cp dist/notification.* DEB.pyNotify.deb/opt/pyNotify/
+cp dist/pyNotify.conf DEB.pyNotify.deb/opt/pyNotify/
+cp dist/pyNotify.conf DEB.pyNotify.deb/etc/
+
+dpkg-deb --build DEB.pyNotify.deb
