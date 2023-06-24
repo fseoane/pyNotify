7759fdc7e1cd3425f245da472865d9179494ec21
fseoane <73739110+fseoane@users.noreply.github.com>
2023-06-24 10:21:08 +0200

    v0.5

--- build.RHEL.sh
+++ build.RHEL.sh
@@ -5,20 +5,7 @@ source .env/bin/activate
 sudo dnf install --assumeyes libappindicator-gtk3
 sudo dnf install --assumeyes pkg-config python3.11-devel
 
-python3.11  -m pip install --upgrade pip setuptools wheel
-
-python3.11  -m pip install --upgrade gotify
-python3.11  -m pip install --upgrade gotify[stream]
-python3.11  -m pip install --upgrade pygame
-python3.11  -m pip install --upgrade pyinstaller
-python3.11  -m pip install --upgrade pycairo
-python3.11  -m pip install --upgrade pygobject
-python3.11  -m pip install --upgrade pystray
-python3.11  -m pip install --upgrade pillow
-python3.11  -m pip install --upgrade websockets
-python3.11  -m pip install --upgrade async
-python3.11  -m pip install --upgrade asyncio
-python3.11  -m pip install --upgrade psutil
+python3.11 -m pip install -r requirements.req
 
 pyinstaller --onefile --windowed pyNotify.py
 
