63d06f7967fb806d966888ef158292ae4b62c34a
fseoane <fseoane@hotmail.com>
2023-06-25 10:18:42 +0200

    v0.6

--- install.binaries.sh
+++ install.binaries.sh
@@ -7,9 +7,9 @@ sudo cp pyNotify.ver /opt/pyNotify/
 sudo cp pyNotify.desktop /opt/pyNotify/
 sudo cp notification.* /opt/pyNotify/
 
-mkdir -p /usr/share/licenses/pyNotify
+sudo mkdir -p /usr/share/licenses/pyNotify
 sudo cp LICENSE /usr/share/licenses/pyNotify/LICENSE
-mkdir -p /usr/share/doc/pyNotify
+sudo mkdir -p /usr/share/doc/pyNotify
 sudo cp README.md /usr/share/doc/pyNotify/README.md
 
 read -r -p "Install default configuration file (pyNotify.conf)? [y/N] " response
