bff255798ea2853e93cacf9255538d2a4c030eaa
fseoane <fseoane@hotmail.com>
2024-01-25 19:29:36 +0100

    ...

--- pyNotify.py
+++ pyNotify.py
@@ -264,7 +264,7 @@ if __name__ == "__main__":
 
 		# Delay 3 minutes to ensure network is ready
 		print("...delayed start (3 minutes) to ensure network is ready")
-		#time.sleep(180)
+		time.sleep(180)
 
 		# Run the gotify listener asynchronously in a second thread
 		with Runner() as runnerGotify, Runner() as runnerNtfy:
