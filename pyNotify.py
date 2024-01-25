d4191d3d6d1fb69444c22d498141222b4cac8d10
fseoane <fseoane@hotmail.com>
2024-01-25 20:03:26 +0100

    ...

--- pyNotify.py
+++ pyNotify.py
@@ -62,7 +62,7 @@ async def log_gotify_push_messages(tray_icon,conf_gotify_url,conf_client_token,c
 	#if __name__ == "__main__":
  	# 	asyncio.run(main())
  
-	print("...listening at {}".format(conf_gotify_url))
+	print("...listening Gotify at {}".format(conf_gotify_url))
 	if (tray_icon.HAS_NOTIFICATION):
 		tray_icon.notify(message="...is ready and listening",title="pyNotify....")
 
@@ -79,7 +79,7 @@ async def log_ntfy_push_messages(tray_icon,conf_ntfy_url,conf_ntfy_topics,conf_n
 	global on_mute
 	global on_dnd 
  
-	print("...listening at {}".format(conf_ntfy_url))
+	print("...listening Ntfy at {}".format(conf_ntfy_url))
 	resp = requests.get("{}/{}/json".format(conf_ntfy_url,conf_ntfy_topics), stream=True)
 	for line in resp.iter_lines():
 		if line:
